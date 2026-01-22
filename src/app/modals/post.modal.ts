3b45c0beb083d3f21b3a254417f40282af86e428
Tawanda <tskasunzuma@gmail.com>
2026-01-22 14:28:57 +0200

    create service and modal for posts

---
+++ src/app/modals/post.modal.ts
@@ -0,0 +1,6 @@
+export interface postModal {
+    postId: Number,
+    title: String,
+    description: String,
+    createdAt: Date
+} 
---
+++ src/app/service/posts.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from '../environments/environments';
+import { map, Observable } from 'rxjs';
+import { postModal } from '../modals/post.modal';
+
+// Makes service available through the entire app
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PostsService {
+
+  // API used to get posts data
+  private apiUrl = `${environment.apiUrl}/posts`;
+
+  // Http is used to send requests to the backend
+  constructor(private http: HttpClient) { }
+
+  // Create new product
+  createPost (post: postModal) : Observable<postModal> {
+    return this.http.post<postModal>(this.apiUrl, post)
+  }
+
+  // Get all posts from the server
+  getAllPosts(): Observable<postModal[]> {
+    return this.http.get<postModal[]>(this.apiUrl)
+  };
+
+  // Get posts by ID
+  getPostById(id: Number): Observable<postModal | undefined> {
+    return this.http.get<postModal>(`${this.apiUrl}/${id}`)
+  };
+
+  // Update post
+  updatePost(id: Number, post: postModal) : Observable<postModal> {
+    return this.http.patch<postModal>(`${this.apiUrl}/${id}`, post);
+  };
+
+  // Delete post
+  deletePost(id: Number) : void | undefined {
+    this.http.delete<postModal>(`${this.apiUrl}/${id}`);
+  };
+}
